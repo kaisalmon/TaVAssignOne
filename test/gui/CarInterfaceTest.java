0307e445e70da32fe7619f22de62d79701f509db
BGextreme <krisikn95@gmail.com>
2016-02-17 14:40:55 +0100

    ReceiveData done!

--- test/gui/CarInterfaceTest.java
+++ test/gui/CarInterfaceTest.java
@@ -6,8 +6,11 @@
 package gui;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import tavassignone.SpeedTorqueObj;
+
+import static org.mockito.Mockito.*;
 
 /**
  *
@@ -17,18 +20,28 @@ public class CarInterfaceTest {
     
     public CarInterfaceTest() {
     }
-
+    
     /**
      * Test of receiveData method, of class CarInterface.
      */
     @Test
     public void testReceiveData() {
+    	
+    	SpeedTorqueObj test2 = mock(SpeedTorqueObj.class);
+    	when(test2.getSpeed()).thenReturn((double) 12);
+    	when(test2.getTorque()).thenReturn((double) 13);
+    	
         System.out.println("receiveData");
-        SpeedTorqueObj expResult = null;
-        SpeedTorqueObj result = CarInterface.receiveData();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        double expResultSpeed = test2.getSpeed();
+        double expResultTorque = test2.getTorque();
+    	
+        double resultOne = CarInterface.receiveData().getSpeed();
+        double resultTwo = CarInterface.receiveData().getTorque();
+        
+        assertEquals(expResultSpeed,0.1, resultOne);
+        assertEquals(expResultTorque,0.1, resultTwo);
+  
     }
 
     /**
