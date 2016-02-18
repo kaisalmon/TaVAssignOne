dcc0f3614c4fbe80efde2dccc3c7d472ffa82b60
BGextreme <krisikn95@gmail.com>
2016-02-18 15:14:36 +0100

    Car fixes

--- src/tavassignone/Car.java
+++ src/tavassignone/Car.java
@@ -8,6 +8,9 @@ public class Car {
 	
 	private SpeedTorque data;
 	private ByteArrayOutputStream stream;
+	private double torque;
+	private double ir;
+	private double uv;
 	
 	public Car(){
 		
@@ -27,4 +30,29 @@ public class Car {
 	public double getTorque(){
 		return data.readSpeedTorque(stream).getTorque();
 	}
+	
+	public void recieveTorque(double torque){
+		this.torque = torque;
+	}
+	
+	public void recieveIrDist(double ir){
+		this.ir = ir;
+	}
+	
+	public void recieveUvDist(double uv){
+		this.uv = uv;
+	}
+	
+	public double checkTorque(){
+		return torque;
+	}
+	
+	public double checkIR(){
+		return ir;
+	}
+	
+	public double checkUV(){
+		return uv;
+	}
+	
 }
