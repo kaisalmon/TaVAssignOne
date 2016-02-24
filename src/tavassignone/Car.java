d0dced57a459a9c69654d1cc5eec45ab4f7ea88f
BGextreme <krisikn95@gmail.com>
2016-02-24 12:53:48 +0100

    fixes to car

--- src/tavassignone/Car.java
+++ src/tavassignone/Car.java
@@ -3,56 +3,34 @@ package tavassignone;
 import static org.mockito.Mockito.*;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class Car {
 	
-	private SpeedTorque data;
-	private ByteArrayOutputStream stream;
-	private double torque;
-	private double ir;
-	private double uv;
+	ByteArrayOutputStream stream;
 	
 	public Car(){
 		
-		   ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		   SpeedTorqueObj dummy = new SpeedTorqueObj(12, 13);
-		   SpeedTorque data = mock(SpeedTorque.class);
-		   when(data.readSpeedTorque(stream)).thenReturn(dummy);
-		   
-		   this.stream = stream;
-		   this.data = data;
+	}
+	
+	public void setStream(ByteArrayOutputStream stream){
+		this.stream = stream;
 	}
 
-	public double getSpeed(){
-	   return data.readSpeedTorque(stream).getSpeed();
+	public ByteArrayOutputStream recieveData(ByteArrayOutputStream sensorData) {
+		return sensorData;
 	}
-	
-	public double getTorque(){
-		return data.readSpeedTorque(stream).getTorque();
-	}
-	
-	public void recieveTorque(double torque){
-		this.torque = torque;
-	}
-	
-	public void recieveIrDist(double ir){
-		this.ir = ir;
-	}
-	
-	public void recieveUvDist(double uv){
-		this.uv = uv;
-	}
-	
-	public double checkTorque(){
-		return torque;
-	}
-	
-	public double checkIR(){
-		return ir;
-	}
-	
-	public double checkUV(){
-		return uv;
+
+	public ByteArrayOutputStream getSpeedTorque() throws IOException {
+		/*
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		
+		byte[] bytes = {9, 63, -16, 0, 0, 0, 0, 0, 0, 10, 64, 20, 0, 0, 0, 0, 0, 0, (byte) (11 - Math.pow(2, 7))};
+		
+		stream.write(bytes);
+		*/
+		
+		return stream;
 	}
 	
 }
